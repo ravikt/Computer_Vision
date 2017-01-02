7d808ead8085598203b834ab7395eb14ccebefda
ravikt <rvthkr4@gmail.com>
2017-01-03 05:23:04 +0530

    update non maximum supression

--- Edge_detection/nonmaxsupress.m
+++ Edge_detection/nonmaxsupress.m
@@ -5,13 +5,35 @@ function [output] = nonmaxsupress(g, theta)
 [m,n]=size(g);
 output=zeros(m,n);
 
-for i=1:m
-   for j=1:n
+for i=2:m
+   for j=2:n
        switch theta(i,j)
            case 0
+               if (g(i,j) >= g(i-1,j)) && ((g(i,j) >= g(i+1,j)))
+                   output(i,j)=255;
+               else
+                   break;
+               end
            case 45
+               if (g(i,j) >= g(i-1,j-1)) && ((g(i,j) >= g(i+1,j+1)))
+                   output(i,j)=255;
+               else
+                   break;
+               end
            case 90
+               if (g(i,j) >= g(i,j+1)) && ((g(i,j) >= g(i,j-1)))
+                   output(i,j)=255;
+               else
+                   break;
+               end
            case 135
+               if (g(i,j) >= g(i-1,j+1)) && ((g(i,j) >= g(i+1,j-1)))
+                   output(i,j)=255;
+               else
+                   break;
+               end
+           otherwise
+               output(i,j)=0;
        end
    end
 end
