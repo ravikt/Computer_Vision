f176ff91f7216d0dd2cd1bca9518c2bb418a980b
ravikt <rvthkr4@gmail.com>
2017-01-09 12:26:15 +0530

    added code for hysteresis

--- Edge_detection/hyster.m
+++ Edge_detection/hyster.m
@@ -1,8 +1,41 @@
-function [ final_image ] = hyster( gh, gl)
+function [ outim ] = hyster( gh, gl)
 %HYSTER Summary of this function goes here
 %   Detailed explanation goes here
+[m,n]=size(gh);
 gl = gl-gh;
-final_image = gl;
+outim = zeros(m,n);% final output image after hysteresis thrsholding
+
+for i=2:m-1
+   for j=2:n-1
+       while gh(i,j) ~= 0
+           outim(i,j)=gh(i,j);
+           if gl(i,j+1) ~= 0 
+               outim(i,j+1) = gl(i,j+1);
+           end
+           if gl(i,j-1) ~= 0
+               outim(i,j-1) = gl(i,j-1); 
+           end
+           if gl(i-1,j) ~= 0
+               outim(i-1,j) = gl(i-1,j);
+           end
+           if gl(i+1,j) ~= 0
+               outim(i+1,j) = gl(i+1,j); 
+           end
+           if gl(i-1,j+1) ~= 0
+               outim(i-1,j+1) = gl(i-1,j+1);
+           end
+           if gl(i+1,j+1) ~= 0
+               outim(i+1,j+1) = gl(i+1,j+1);
+           end
+           if gl(i-1,j-1) ~= 0
+               outim(i-1,j-1) = gl(i-1,j-1);
+           end
+           if gl(i+1,j-1) ~= 0
+               outim(i+1,j-1) = gl(i+1,j-1);
+           end
+       end
+   end
+end
 
 end
 
