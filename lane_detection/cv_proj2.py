aa070fcb155c1a918fdd6bdbecd179effcfa38bc
ravikt <rvthkr4@gmail.com>
2017-05-02 17:01:08 +0530

    update to lane code

--- lane_detection/cv_proj2.py
+++ lane_detection/cv_proj2.py
@@ -8,44 +8,62 @@ import skvideo.datasets
 inVideo = skvideo.io.vreader('lane_vid.mp4')
 #video = skvideo.utils.rgb2gray('lane_vid.mp4')
 
+# def regint(im):
+#         [m, n] = im.shape
+#         mask = np.zeros((m, n), dtype=np.uint8);
+
+
 def conversion(im):
-        outimage = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+        outimage = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         #outimage = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
         return outimage
 
 def canny(im):
         incanny = cv2.GaussianBlur(im, (5, 5), 0)
-        outimage =  cv2.Canny(incanny, 100, 200)
+        outimage = cv2.Canny(incanny, 100, 200)
         return outimage
+
+def draw(img, lines):
+
+  if lines is not None:
+      for line in lines:
+        x1, y1, x2, y2 = line[0]
+      cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255, 2)
+
+
+def hough(img):
+        lines = cv2.HoughLinesP(img, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
+        limg = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+        draw(limg, lines)
+        return limg
+
+def blendim(img1, img2):
+        out = cv2.add(img1, img2)
+        return out
 
 def operation(im):
         inimage = im
         gray = conversion(inimage)
-        outimage = canny(gray)
+        edge = canny(gray)
+        outimage = hough(edge)
+        #outimage = conversion(outimage)
+        outimage = blendim(outimage, inimage)
         return outimage
 
-# outputdata = np.random.random(size=(500, 360, 490, 1)) * 255
-# outputdata = outputdata.astype(np.uint8)
-outVideo = np.empty((5291, 360, 490))
+outVideo = np.empty([5291, 360, 490, 3], dtype = np.uint8)
 outVideo = outVideo.astype(np.uint8)
+#outVideo = np.empty((5291, 360, 490))
+#outVideo = outVideo.astype(np.uint8)
 i = 0
 
 for frame in inVideo:
         #print(frame.shape)
-
         outVideo[i] = operation(frame)
         i = i+1;
+
 #plt.imshow(frame)
 #plt.imshow(output)
 #plt.show()
 skvideo.io.vwrite("outputvideo1.mp4", outVideo)
 
-
-<<<<<<< HEAD
-=======
-skvideo.io.vwrite("outputvideo.mp4", outVideo)
-
-
-
->>>>>>> c2e3acc80308761eec9f84a485317bae71406acb
 
