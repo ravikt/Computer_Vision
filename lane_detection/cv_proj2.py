c9766e31a802ac657c1fed04ee1f16f1284878e3
ravikt <rvthkr4@gmail.com>
2017-05-02 22:30:07 +0530

    update to lane code

--- lane_detection/cv_proj2.py
+++ lane_detection/cv_proj2.py
@@ -4,10 +4,11 @@ import numpy as np
 import skvideo.io
 import skvideo.utils
 import skvideo.datasets
-#inVideo = skvideo.io.vread('lane_vid.mp4')
-inVideo = skvideo.io.vreader('lane_vid.mp4')
+inVideo = skvideo.io.vread('lane_vid.mp4')
+#inVideo = skvideo.io.vreader('title.avi')
 #video = skvideo.utils.rgb2gray('lane_vid.mp4')
 
+# For extracting relevant regions in the video
 # def regint(im):
 #         [m, n, k] = im.shape
 #         mask = np.zeros((m, n, k), dtype=np.uint8);
@@ -19,32 +20,41 @@ inVideo = skvideo.io.vreader('lane_vid.mp4')
 #              break;
 #         return mask;
 
+# Conversion of frames from RGB to grayscale
 def conversion(im):
         outimage = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         #outimage = cv2.cvtColor(im, cv2.COLOR_RGB2HSV)
         return outimage
 
+# Detects edges using canny edge detection algorithm
 def canny(im):
         incanny = cv2.GaussianBlur(im, (5, 5), 0)
         outimage = cv2.Canny(incanny, 30, 130)
         return outimage
 
+# def slope(line):
+#     return (float(line[3]) - line[1]) / (float(line[2]) - line[0])
+
+# Draws the detected line
 def draw(img, lines):
       if lines is not None:
         for line in lines:
          x1, y1, x2, y2 = line[0]
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
 
+# Detects the lines in the images
 def hough(img):
         lines = cv2.HoughLinesP(img, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
         limg = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
         draw(limg, lines)
         return limg
 
+# Add two detected lines to original image
 def blendim(img1, img2):
         out = cv2.add(img1, img2)
         return out
 
+# Calls all the functions sequentially
 def operation(im):
         inimage = im
         #inimage = regint(im)
@@ -55,6 +65,7 @@ def operation(im):
         outimage = blendim(outimage, im)
         return outimage
 
+# An empty matrix to hold all the sequence of images
 outVideo = np.empty([5291, 360, 490, 3], dtype = np.uint8)
 outVideo = outVideo.astype(np.uint8)
 #outVideo = np.empty((5291, 360, 490))
@@ -69,7 +80,10 @@ for frame in inVideo:
 #plt.imshow(frame)
 #plt.imshow(output)
 #plt.show()
+
+# Writes the the output image sequences in a video file
 skvideo.io.vwrite("outputvideo1.mp4", outVideo)
+
 
 
 
